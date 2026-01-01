5f522cf3cac36bffcd65d4d5be514e11ebda2ff2
AnikAhmed <anikahamedofc123@gmail.com>
2026-01-01 14:10:16 +0600

    echo "# oop_dart" >> README.md

---
+++ bin/oop_dart.dart
@@ -0,0 +1,21 @@
+import 'package:oop_dart/camera_oop.dart';
+
+void main() {
+  Camera cameraOne = Camera();
+  Camera cameraTwo = Camera();
+
+  cameraOne.id = 01;
+  cameraOne.cameraName = "Canon EOS 2000D DSLR Camera (Body Only)";
+  cameraOne.megaPixel = "24.1MP APS-C CMOS Sensor";
+  cameraOne.model = "Canon 2000D";
+  cameraOne.price = 38000;
+
+  cameraTwo.id = 02;
+  cameraTwo.cameraName = "Canon EOS 3000D 18MP DSLR Camera With EF-S 18-55mm III Lens";
+  cameraTwo.megaPixel = "18-megapixel APS-C-size CMOS sensor & DIGIC 4+ image processor";
+  cameraTwo.model = "EOS 3000D";
+  cameraTwo.price = 41000;
+
+  cameraOne.getCamera();
+  cameraTwo.getCamera();
+}
---
+++ lib/camera_oop.dart
@@ -0,0 +1,18 @@
+class Camera{
+  int? id;
+  String? cameraName;
+  String? megaPixel;
+  String? model;
+  double? price;
+
+
+  void getCamera(){
+    print("Id is: $id");
+    print("cameraName is: $cameraName");
+    print("megaPixel is: $megaPixel");
+    print("model is: $model");
+    print("price is: $price");
+    print("====================");
+
+  }
+}
